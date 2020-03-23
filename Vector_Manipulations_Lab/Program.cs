71be17f888a90422010b080068e6a7f835441d5f
Taras <tariksyurts@gmail.com>
2020-03-23 08:19:25 +0200

    init

---
+++ Vector_Manipulations_Lab/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_Manipulations_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vector<int> vector = new Vector<int>();
+
+            Console.WriteLine("Створив пустий вектор: "+vector);
+
+            AddRandValuesToVector(vector, 20, -10, 15);
+            
+            Console.WriteLine("Добавлено випадковi значення: \n" + vector);
+            Console.WriteLine("Розмiр: "+vector.Size());
+            Console.WriteLine("--------------------------------");
+
+            int indBeg = -1, indEnd = -1;
+
+            GetLongestOddIndexes(vector, out indBeg, out indEnd);
+
+            Console.WriteLine("Найдовша послiдовнiсть непарних чисел. Початок: " + indBeg + "  Кiнець:" + indEnd);
+
+            vector.RemoveRange(indBeg, indEnd);
+
+            Console.WriteLine("Видалено послiдовнiсть: \n" + vector);
+            Console.WriteLine("Розмiр: " + vector.Size());
+            Console.WriteLine("--------------------------------");
+
+            DublicateOddValues(vector);
+
+            Console.WriteLine("Подвоєно непарнi: \n" + vector);
+            Console.WriteLine("Розмiр: " + vector.Size());
+            Console.WriteLine("--------------------------------");
+
+            var newVector = RearrangeOddAndEven(vector);
+
+            Console.WriteLine("Новий вектор: \n" + newVector);
+        }
+
+        static Vector<int> RearrangeOddAndEven(Vector<int> vector)
+        {
+            Vector<int> newVector = new Vector<int>();
+
+            for(int i=0;i< vector.Size(); i++)
+            {
+                if(vector.Get(i) % 2 != 0)
+                {
+                    newVector.Add(vector.Get(i));
+                }
+            }
+            for (int i = 0; i < vector.Size(); i++)
+            {
+                if (vector.Get(i) % 2 == 0)
+                {
+                    newVector.Add(vector.Get(i));
+                }
+            }
+
+            return newVector;
+        }
+
+        static void DublicateOddValues(Vector<int> vector)
+        {
+            for(int i = 0; i < vector.Size(); i++)
+            {
+                if (vector.Get(i) % 2 != 0)
+                {
+                    vector.Add(vector.Get(i), i);
+                    i++;
+                }
+            }
+        }
+
+        static void GetLongestOddIndexes(Vector<int> vector, out int indexBeg, out int indexEnd)
+        {
+            indexBeg = -1;
+
+            int currLenght = 0;
+            int maxLenght = 0;
+            int currLenghtBeg = 0;
+
+            for (int i = 0; i < vector.Size(); i++){
+                if (vector.Get(i) % 2 == 0) // EVEN
+                {
+                    if (currLenght > maxLenght){
+                        indexBeg = currLenghtBeg;
+                        maxLenght = currLenght;
+                    }
+                    currLenght = 0;
+                }                           // ODD
+                else{
+                    if (currLenght == 0)
+                    {
+                        currLenghtBeg = i;
+                    }
+                    currLenght++;
+                }
+            }
+            if (currLenght > maxLenght)
+            {
+                indexBeg = currLenghtBeg;
+                maxLenght = currLenght;
+            }
+
+            indexEnd = indexBeg + maxLenght-1;
+
+
+        }
+
+        static void AddRandValuesToVector(Vector<int> vector, int amount, int min, int max)
+        {
+            Random random = new Random();
+            
+            for(int i = 0; i < amount; i++)
+            {
+                vector.Add(random.Next(min, max + 1));
+            }
+        }
+
+    }
+}
---
+++ Vector_Manipulations_Lab/Vector.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_Manipulations_Lab
+{
+    class Vector<T>
+    {
+        private T[] array;
+        private int nextIndex;
+
+        public Vector() {
+            array = new T[1];
+            nextIndex = 0;
+        }
+
+        public Vector(int size)
+        {
+            array = new T[size];
+            nextIndex = 0;
+        }
+
+        private void UpsizeArray(int num)
+        {
+            T[] tempArray = new T[array.Length + num];
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            array = tempArray;
+        }
+
+        public T Get(int index)
+        {
+            if (index < nextIndex)
+            {
+                return array[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public void Add(T value)
+        {
+            if (value != null)
+            {
+                if (nextIndex >= array.Length) { UpsizeArray(5); }
+
+                array[nextIndex] = value;
+                nextIndex++;
+            }
+        }
+
+        public void Add(T value,int index)
+        {
+            if (value != null && index < nextIndex)
+            {
+                if (nextIndex >= array.Length) { UpsizeArray(5); }
+
+                for(int i = nextIndex - 1; i >= index; i--)
+                {
+                    array[i + 1] = array[i];
+                }
+                array[index] = value;
+                nextIndex++;
+            }
+        }
+
+
+        public void Remove(int index)
+        {
+            if (index < nextIndex)
+            {
+                for(int i = index; i < nextIndex-1; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                nextIndex--;
+            }
+        }
+
+        public void RemoveRange(int index1, int index2)
+        {
+            if (index2 < nextIndex) {
+
+                int tempIndex = index1;
+
+                for (int i = index2+1; i < nextIndex; i++)
+                {
+                    array[tempIndex] = array[i];
+                    tempIndex++;
+                }
+
+                nextIndex = tempIndex;
+            }
+        }
+
+        public int Size()
+        {
+            return nextIndex;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+
+            s += "Vector: (";
+            for (int i = 0; i < nextIndex; i++)
+            {
+                s += array[i].ToString();
+
+                if (i != nextIndex - 1)
+                {
+                    s += " ,";
+                }
+            }
+            s += " )";
+            return s;
+        }
+    }
+}
